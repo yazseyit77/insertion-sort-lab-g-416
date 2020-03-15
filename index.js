b09b642a5af4a9b50b273d196d638a4dcc99eec4
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-15 18:55:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ function findMinAndRemove(array){
   let minIndex = 0;
   for(let i = 0; i < array.length; i++){
     if(minArr > array[i]){
-      minArr = array[i]
+      minArr = array[i];
+      minIndex = i;
     }
+    return minArr;
   }
 }
 
