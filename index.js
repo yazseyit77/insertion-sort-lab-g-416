11c210976803c44e5988a67e5a5e72b1de64b3d0
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-03-15 19:09:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function findMinAndRemove(array){
-
+  let minArr = array[0];
+  let minIndex = 0;
+  for(let i = 0; i < array.length; i++){
+    let element = array[i];
+    if(minArr > array[i]){
+      minArr = array[i];
+      minIndex = i;
+    }
+  }
+  array.splice(minIndex, 1)
+  return minArr;
 }
 
-function insertionSort(array){
 
+
+
+function insertionSort(array){
+  let newArr;
+  let sortedArr = [];
+  do{
+    newArr = findMinAndRemove(array);
+    sortedArr.push(newArr);
+  }while(array.length != 0)
+  return sortedArr;
 }
